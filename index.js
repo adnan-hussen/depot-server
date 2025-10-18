863c24db93e27fbea50f50c3a1bc5430dba3cee1
Adnan Hussen <adnan.abdulfetah.hussen@gmail.com>
2025-10-18 18:12:46 +0400

    fix: change cors origin

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://depot-theta.vercel.app",
     credentials:true,
 }));
 
